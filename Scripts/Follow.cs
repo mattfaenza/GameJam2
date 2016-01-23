2867cfe2ccc5d921d1985ac0ae946858e5b27647
alextag <alex.tagtalenidis@mail.utoronto.ca>
2016-01-23 12:11:02 -0500

    Added Enemy, following AI and explosion logic

---
+++ Scripts/Follow.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour {
+
+    GameObject toFollow;
+    private Rigidbody rb;
+    public float speed;
+    // Use this for initialization
+    void Start () {
+        toFollow = null;
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log(toFollow);
+        if (toFollow != null) {
+            //Debug.Log("I've got you!");
+            float step = speed * Time.deltaTime;
+            Vector3 movement = Vector3.MoveTowards(rb.transform.localPosition,toFollow.transform.localPosition,step);
+            transform.position = movement;
+            //Debug.Log(movement);
+            //rb.AddForce(movement);
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            toFollow = other.gameObject;
+            //Debug.Log("Found you!");
+            //Debug.Log(toFollow);
+        }
+        //Debug.Log("Collided with the ground!");
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            toFollow = null;
+            //Debug.Log("Lost you!");
+        }
+        //Debug.Log("Collided with the ground!");
+    }
+
+}
---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    // Use this for initialization
+    private Rigidbody rb;
+    public float speed;
+    public float jump_height;
+	void Start () {
+        //Debug.Log("Hello Scripting!");
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(speed * horizontal, 0, speed * vertical);
+        rb.AddForce(movement);
+    }
+
+    // Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnTriggerEnter(Collider other) {
+
+        if (other.CompareTag("Enemy"))
+        {
+            Debug.Log("EXPLOSION!");
+        }
+    }
+}
